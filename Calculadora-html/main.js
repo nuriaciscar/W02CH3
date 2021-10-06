43b5c8447731b45b9226c01105481958d1bccffc
nuriaciscar <nuriaciscar@hotmail.com>
2021-10-06 21:04:25 +0200

    Tried to merge to master

--- Calculadora-html/main.js
+++ Calculadora-html/main.js
@@ -1,101 +1,95 @@
+// Selectores HTML
+const botonNumeros = document.querySelectorAll(".numero"); // Todos los numeros
+const botonBorrar = document.getElementById(".ac"); // TBoton de borrar
+const botonOperador = document.getElementById("operador");
+const botonResultado = document.getElementById(".igual");
 
-//Selectores HTML
-const botonNumeros=document.querySelectorAll('.numero'); //Todos los numeros
-const botonBorrar= document.getElementById('.ac'); //TBoton de borrar
-const botonOperador= document.getElementById('operador');
-const botonResultado= document.getElementById('.igual');
+const pantalla = document.getElementById("resultado");
 
-let pantalla= document.getElementById('resultado'); 
+// Estado
+let operacionActual = "";
+let operacionAnterior = ""; // La guardamos para poder hacer más de una operación
+let operacion;
 
-
-//Estado
-let operacionActual= '';
-let operacionAnterior= ''; //La guardamos para poder hacer más de una operación
-let operacion= undefined;
-
-//Eventos
-botonNumeros.forEach(function(boton){
-    boton.addEventListener('click',function(){
+// Eventos
+botonNumeros.forEach((boton) => {
+    boton.addEventListener("click", () => {
         añadirNumero(boton.innerText);
-    })
-
-});
-
-botonOperador.forEach(function(boton){
-    boton.addEventListener('click', function(){
-        tipoOperacion(boton.innerText);
-
     });
-
 });
 
-botonResultado.addEventListener('click', function(){
+botonOperador.forEach((boton) => {
+    boton.addEventListener("click", () => {
+        tipoOperacion(boton.innerText);
+    });
+});
+
+botonResultado.addEventListener("click", () => {
     calcular();
     actualizarValor();
 });
 
-botonBorrar.addEventListener('click', function(){
+botonBorrar.addEventListener("click", () => {
     limpiar();
     actualizarValor();
 });
 
-function tipoOperacion(op){
-    if (operacionActual===''){
+function tipoOperacion(op) {
+    if (operacionActual === "") {
         return;
-    } else{
-        calcular();
     }
-    operacion= op.toString();
-    operacionAnterior= operacionActual;
-    operacionActual='';
+    calcular();
+
+    operacion = op.toString();
+    operacionAnterior = operacionActual;
+    operacionActual = "";
 }
 
-function calcular(){
+function calcular() {
     let calculo;
-    const a= parseFloat(operacionAnterior);
-    const b=parseFloat(operacionActual);
-    if (isNaN(a) || isNaN(b)) return; //Si son numericos los dos
-    
-    switch(operacion){
-        case '+':
-            calculo= a + b;
+    const a = parseFloat(operacionAnterior);
+    const b = parseFloat(operacionActual);
+    if (Number.isNaN(a) || Number.isNaN(b)) return; // Si son numericos los dos
+
+    switch (operacion) {
+        case "+":
+            calculo = a + b;
             break;
-        case '-':
-            calculo= a - b;
+        case "-":
+            calculo = a - b;
             break;
-        case 'x':
-            calculo= a * b;
+        case "x":
+            calculo = a * b;
             break;
-        case '÷':
-            calculo= a / b;
+        case "÷":
+            calculo = a / b;
             break;
-        case '%':
-            calculo= a % b;
+        case "%":
+            calculo = a % b;
             break;
         default:
-            console.log('Error');
+            console.log("Error");
             return;
-
     }
-    operacionActual= calculo;
-    operacion= '';
-    operacionAnterior= '';
+    operacionActual = calculo;
+    operacion = "";
+    operacionAnterior = "";
 }
-function añadirNumero(num){ //Creamos una variable num 
-    if(num=== '.' && operacionActual.includes('.')) return;
-    operacionActual= operacionActual.toString() + num.toString(); //Lo pasamos a texto
+function añadirNumero(num) {
+    // Creamos una variable num
+    if (num === "." && operacionActual.includes(".")) return;
+    operacionActual = operacionActual.toString() + num.toString(); // Lo pasamos a texto
     actualizarValor();
 }
 
-function limpiar(){
-    operacionActual= '';
-    operacionAnterior= '';
-    operacion= undefined;
+function limpiar() {
+    operacionActual = "";
+    operacionAnterior = "";
+    operacion = undefined;
 }
 
-function actualizarValor(){
-    pantalla.value= operacionActual;
-
+function actualizarValor() {
+    pantalla.value = operacionActual;
 }
 
 limpiar();
